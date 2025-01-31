0980ed141025e2bc28615afe11db13c70cc6bb0b
Rashmi Pathinayaka <140165202+rashmipathinayaka@users.noreply.github.com>
2025-01-31 11:25:24 +0530

    create lanowner view

---
+++ app/views/Landowner/sidebar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/components/sidebar.css">
+
+    <title>Document</title>
+</head>
+<body>
+<div class="sidebar">
+<ul>
+    <li><a href="<?php echo URLROOT; ?>/Landowner/index">Dashboard</a></li>
+    <li><a href="<?php echo URLROOT; ?>/Landowner/registerland">Register a Land</a></li>
+    <li><a href="<?php echo URLROOT; ?>/Landowner/manageland">Manage Lands</a></li>
+    <li><a href="<?php echo URLROOT; ?>/gl/marketplace">Marketplace</a></li>
+</ul>
+
+
+            <ul class="logout">
+                <li><a href="#">Log Out</a></li>
+            </ul>
+        </div>
+</body>
+</html>
